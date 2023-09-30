8cb107b509bd480007f70909499821e69ef398f3
Amycus89 <amycus89@gmail.com>
2023-09-30 20:51:11 +0200

    Make it find location based on IP by default

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-import requests
-import sqlite3
 from datetime import datetime
 from flask import Flask, render_template, request
+import requests
+import json
+import sqlite3
 
 # Configure application
 app = Flask(__name__, template_folder="templates")
@@ -76,13 +77,82 @@ def index():
             'sun_moon': sun_moon
         }
 
+        # Return weather_info back to the browser
+        return render_template('index.html', weather_info=weather_info)
+    else:
+        # Request for the user's IP address
+        ip = requests.get('https://api64.ipify.org?format=json').json()
+        # Store the IP address in a variable
+        ip_address = ip['ip']
+
+        location_data = requests.get(f'http://ip-api.com/json/{ip_address}?fields=status,message,country,regionName,city,lat,lon,offset').json()
+
+        # Save all the info needed in their own variables
+
+        latitude = location_data['lat']
+        longitude = location_data['lon']
+        # Timezone offset in seconds:
+        timezone = location_data['offset']
+
+        # Obtain a working url to find weather info based on user's location
+        coordinate_api_url = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={key}"
+        # Request and store the data from it
+        weather_request=requests.get(coordinate_api_url)
+        weather_data = weather_request.json()
+
+
+        # Save all the info needed in a variable
+        weather_description = weather_data['weather'][0]['description']
+        # Get temperature in Kelvin
+        temp = weather_data['main']['temp']
+        temp_min = weather_data['main']['temp_min']
+        temp_max = weather_data['main']['temp_max']
+        feels_like = weather_data['main']['feels_like']
+        humidity = weather_data['main']['humidity']
+        pressure = weather_data['main']['pressure']
+        visibility = weather_data['visibility']
+        wind_speed = weather_data['wind']['speed']
+        wind_deg = weather_data['wind']['deg']
+        name = weather_data['name']
+        country = weather_data['sys']['country']
+        sunrise = weather_data['sys']['sunrise']
+        sunset = weather_data['sys']['sunset']
+        id = weather_data['id']
+
+        # Check if it's day or night
+        sun_moon = ''
+        if sunrise < sunset:
+            sun_moon = 'sun'
+        else:
+            sun_moon = 'moon'
+
+        # Convert from Unix timestamp format to readable format
+        sunrise = datetime.utcfromtimestamp(sunrise).strftime('%Y-%m-%d %H:%M:%S')
+        sunset = datetime.utcfromtimestamp(sunset).strftime('%Y-%m-%d %H:%M:%S')
+
+        # Store all of the info in a dictionary
+        weather_info = {
+            'weather_description': weather_description,
+            'temp': temp,
+            'temp_min': temp_min,
+            'temp_max': temp_max,
+            'feels_like': feels_like,
+            'humidity': humidity,
+            'pressure': pressure,
+            'visibility': visibility,
+            'wind_speed': wind_speed,
+            'wind_deg': wind_deg,
+            'name': name,
+            'country': country,
+            'sunrise': sunrise,
+            'sunset': sunset,
+            'id': id,
+            'sun_moon': sun_moon
+        }
+
+        # convert dictionary to JSON
+        weather_info = json.dumps(weather_info)
         print(weather_info)
 
         # Return weather_info back to the browser
         return render_template('index.html', weather_info=weather_info)
-    else:
-        return render_template('index.html')
-
-
-
-# https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}

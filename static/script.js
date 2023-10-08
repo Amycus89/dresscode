4534df1184b3971066690a49c3d5299028a1bf74
Amycus89 <amycus89@gmail.com>
2023-10-08 19:26:11 +0200

    Make weather icon update depending on weather and time

--- static/script.js
+++ static/script.js
@@ -43,7 +43,200 @@ const updateClock = function (timeZone) {
     let seconds = String(localTime.getSeconds()).padStart(2, "0");
     let timestring = hours + ":" + minutes + ":" + seconds;
     document.getElementById("clock").innerHTML = timestring;
+    return localTime;
   }, 1000);
+};
+
+// Function to determine day or night
+const updateWeatherIcon = function (id, currentTime, sunrise, sunset) {
+  console.log(id);
+  // Convert current time to Unix timestamp format
+  currentTime = Math.floor(Date.now() / 1000);
+  let sunMoon = "";
+  if (currentTime > sunrise && currentTime < sunset) {
+    sunMoon = "sun";
+    console.log(currentTime);
+  } else {
+    sunMoon = "moon";
+  }
+
+  switch (sunMoon) {
+    case "sun":
+      switch (true) {
+        case id > 199 && id < 203:
+          weatherIconElement.src =
+            "/static/weatherIcons/thunderstorms-day-rain.svg";
+          weatherIconElement.alt = "Day with thunderstorm and rain";
+          break;
+        case id > 299 && id < 322:
+          weatherIconElement.src = "/static/weatherIcons/thunderstorms-day.svg";
+          weatherIconElement.alt = "Day with thunderstorm";
+          break;
+        case id > 229 && id < 233:
+          weatherIconElement.src =
+            "/static/weatherIcons/thunderstorms-day-snow.svg";
+          weatherIconElement.alt = "Day with thunderstorm and drizzle";
+          break;
+        case id > 299 && id < 322:
+          weatherIconElement.src =
+            "/static/weatherIcons/partly-cloudy-day-drizzle.svg";
+          weatherIconElement.alt = "Day with drizzle";
+          break;
+        case id > 499 && id < 532:
+          weatherIconElement.src =
+            "/static/weatherIcons/partly-cloudy-day-rain.svg";
+          weatherIconElement.alt = "Day with rain";
+          break;
+        case id > 599 && id < 603:
+          weatherIconElement.src =
+            "/static/weatherIcons/partly-cloudy-day-snow.svg";
+          weatherIconElement.alt = "Day with snow";
+          break;
+        case id > 610 && id < 614:
+          weatherIconElement.src =
+            "/static/weatherIcons/partly-cloudy-day-sleet.svg";
+          weatherIconElement.alt = "Day with sleet";
+          break;
+        case id > 614 && id < 623:
+          weatherIconElement.src =
+            "/static/weatherIcons/partly-cloudy-day-sleet.svg";
+          weatherIconElement.alt = "Day with rain and snow";
+          break;
+        case id > 700 && id < 703:
+          weatherIconElement.src = "/static/weatherIcons/mist.svg";
+          weatherIconElement.alt = "Day with mist";
+          break;
+        case id > 710 && id < 712:
+          weatherIconElement.src = "/static/weatherIcons/smoke-particles.svg";
+          weatherIconElement.alt = "Day with smoke particles";
+          break;
+        case id > 720 && id < 723:
+          weatherIconElement.src = "/static/weatherIcons/haze-day.svg";
+          weatherIconElement.alt = "Day with haze";
+          break;
+        case (id > 730 && id < 732) || id == 761:
+          weatherIconElement.src = "/static/weatherIcons/dust-day.svg";
+          weatherIconElement.alt = "Day with dust whirls";
+          break;
+        case id > 740 && id < 742:
+          weatherIconElement.src = "/static/weatherIcons/fog-day.svg";
+          weatherIconElement.alt = "Day with fog";
+          break;
+        case id == 751:
+          weatherIconElement.src = "/static/weatherIcons/dust-day.svg";
+          weatherIconElement.alt = "Day with sand storms";
+          break;
+        case id == 762:
+          weatherIconElement.src = "/static/weatherIcons/ash.svg";
+          weatherIconElement.alt = "Day with ash";
+          break;
+        case id == 771:
+          weatherIconElement.src = "/static/weatherIcons/squall.svg";
+          weatherIconElement.alt = "Day with squalls";
+          break;
+        case id == 781:
+          weatherIconElement.src = "/static/weatherIcons/tornado.svg";
+          weatherIconElement.alt = "Day with tornado";
+          break;
+        case id == 800:
+          weatherIconElement.src = "/static/weatherIcons/clear-day.svg";
+          weatherIconElement.alt = "Day with clear sky";
+          break;
+        default:
+          weatherIconElement.src = "/static/weatherIcons/partly-cloudy-day.svg";
+          weatherIconElement.alt = "Cloudy day";
+          break;
+      }
+      break;
+    case "moon":
+      switch (true) {
+        case id > 199 && id < 203:
+          weatherIconElement.src =
+            "/static/weatherIcons/thunderstorms-night-rain.svg";
+          weatherIconElement.alt = "Night with thunderstorm and rain";
+          break;
+        case id > 299 && id < 322:
+          weatherIconElement.src =
+            "/static/weatherIcons/thunderstorms-night.svg";
+          weatherIconElement.alt = "Night with thunderstorm";
+          break;
+        case id > 229 && id < 233:
+          weatherIconElement.src =
+            "/static/weatherIcons/thunderstorms-night-snow.svg";
+          weatherIconElement.alt = "Night with thunderstorm and drizzle";
+          break;
+        case id > 299 && id < 322:
+          weatherIconElement.src =
+            "/static/weatherIcons/partly-cloudy-night-drizzle.svg";
+          weatherIconElement.alt = "Night with drizzle";
+          break;
+        case id > 499 && id < 532:
+          weatherIconElement.src =
+            "/static/weatherIcons/partly-cloudy-night-rain.svg";
+          weatherIconElement.alt = "Night with rain";
+          break;
+        case id > 599 && id < 603:
+          weatherIconElement.src =
+            "/static/weatherIcons/partly-cloudy-night-snow.svg";
+          weatherIconElement.alt = "Night with snow";
+          break;
+        case id > 610 && id < 614:
+          weatherIconElement.src =
+            "/static/weatherIcons/partly-cloudy-night-sleet.svg";
+          weatherIconElement.alt = "Night with sleet";
+          break;
+        case id > 614 && id < 623:
+          weatherIconElement.src =
+            "/static/weatherIcons/partly-cloudy-night-sleet.svg";
+          weatherIconElement.alt = "Night with rain and snow";
+          break;
+        case id > 700 && id < 703:
+          weatherIconElement.src = "/static/weatherIcons/mist.svg";
+          weatherIconElement.alt = "Night with mist";
+          break;
+        case id > 710 && id < 712:
+          weatherIconElement.src = "/static/weatherIcons/smoke-particles.svg";
+          weatherIconElement.alt = "Night with smoke particles";
+          break;
+        case id > 720 && id < 723:
+          weatherIconElement.src = "/static/weatherIcons/haze-night.svg";
+          weatherIconElement.alt = "Night with haze";
+          break;
+        case (id > 730 && id < 732) || id == 761:
+          weatherIconElement.src = "/static/weatherIcons/dust-night.svg";
+          weatherIconElement.alt = "Night with dust whirls";
+          break;
+        case id > 740 && id < 742:
+          weatherIconElement.src = "/static/weatherIcons/fog-night.svg";
+          weatherIconElement.alt = "Night with fog";
+          break;
+        case id == 751:
+          weatherIconElement.src = "/static/weatherIcons/dust-night.svg";
+          weatherIconElement.alt = "Night with sand storms";
+          break;
+        case id == 762:
+          weatherIconElement.src = "/static/weatherIcons/ash.svg";
+          weatherIconElement.alt = "Night with ash";
+          break;
+        case id == 771:
+          weatherIconElement.src = "/static/weatherIcons/squall.svg";
+          weatherIconElement.alt = "Night with squalls";
+          break;
+        case id == 781:
+          weatherIconElement.src = "/static/weatherIcons/tornado.svg";
+          weatherIconElement.alt = "Night with tornado";
+          break;
+        case id == 800:
+          weatherIconElement.src = "/static/weatherIcons/starry-night.svg";
+          weatherIconElement.alt = "Night with clear sky";
+          break;
+        default:
+          weatherIconElement.src =
+            "/static/weatherIcons/partly-cloudy-night.svg";
+          weatherIconElement.alt = "Cloudy night";
+          break;
+      }
+  }
 };
 
 // Function to load history shortcut buttons
@@ -85,7 +278,10 @@ const loadHistory = function (searchData) {
         .then((response) => response.json())
         .then((data) => {
           updateWeatherInfo(data);
-          updateClock(data.timezone);
+          currentTime = updateClock(data.timezone);
+          let sunrise = data.sys.sunrise;
+          let sunset = data.sys.sunset;
+          updateWeatherIcon(data.weather[0].id, currentTime, sunrise, sunset);
         });
     });
   });
@@ -114,7 +310,10 @@ document.getElementById("getLocation").addEventListener("click", function () {
         .then((response) => response.json())
         .then((data) => {
           updateWeatherInfo(data);
-          updateClock(data.timezone);
+          currentTime = updateClock(data.timezone);
+          let sunrise = data.sys.sunrise;
+          let sunset = data.sys.sunset;
+          updateWeatherIcon(data.weather[0].id, currentTime, sunrise, sunset);
         });
     });
   } else {
@@ -127,148 +326,15 @@ document.getElementById("getLocation").addEventListener("click", function () {
 document.addEventListener("DOMContentLoaded", function () {
   // Setup time
   intervalId = null;
-  updateClock(weatherInfo.timezone);
+  let currentTime = updateClock(weatherInfo.timezone);
+  let sunrise = weatherInfo.sys.sunrise;
+  let sunset = weatherInfo.sys.sunset;
+  updateWeatherIcon(weatherInfo.weather[0].id, currentTime, sunrise, sunset);
+
   // Setup history shortcut buttons
   loadHistory(searches);
 
   // placeholders
-  let sunMoon = "sun";
-  let id = 200;
-
-  if (sunMoon == "sun") {
-    if (id > 199 && id < 203) {
-      weatherIconElement.src =
-        "/static/weatherIcons/thunderstorms-day-rain.svg";
-      weatherIconElement.alt = "Day with thunderstorm and rain";
-    } else if (id > 299 && id < 322) {
-      weatherIconElement.src = "/static/weatherIcons/thunderstorms-day.svg";
-      weatherIconElement.alt = "Day with thunderstorm";
-    } else if (id > 229 && id < 233) {
-      weatherIconElement.src =
-        "/static/weatherIcons/thunderstorms-day-snow.svg";
-      weatherIconElement.alt = "Day with thunderstorm and drizzle";
-    } else if (id > 299 && id < 322) {
-      weatherIconElement.src =
-        "/static/weatherIcons/partly-cloudy-day-drizzle.svg";
-      weatherIconElement.alt = "Day with drizzle";
-    } else if (id > 499 && id < 532) {
-      weatherIconElement.src =
-        "/static/weatherIcons/partly-cloudy-day-rain.svg";
-      weatherIconElement.alt = "Day with rain";
-    } else if (id > 599 && id < 603) {
-      weatherIconElement.src =
-        "/static/weatherIcons/partly-cloudy-day-snow.svg";
-      weatherIconElement.alt = "Day with snow";
-    } else if (id > 610 && id < 614) {
-      weatherIconElement.src =
-        "/static/weatherIcons/partly-cloudy-day-sleet.svg";
-      weatherIconElement.alt = "Day with sleet";
-    } else if (id > 614 && id < 623) {
-      weatherIconElement.src =
-        "/static/weatherIcons/partly-cloudy-day-sleet.svg";
-      weatherIconElement.alt = "Day with rain and snow";
-    } else if (id > 700 && id < 703) {
-      weatherIconElement.src = "/static/weatherIcons/mist.svg";
-      weatherIconElement.alt = "Day with mist";
-    } else if (id > 710 && id < 712) {
-      weatherIconElement.src = "/static/weatherIcons/smoke-particles.svg";
-      weatherIconElement.alt = "Day with smoke particles";
-    } else if (id > 720 && id < 723) {
-      weatherIconElement.src = "/static/weatherIcons/haze-day.svg";
-      weatherIconElement.alt = "Day with haze";
-    } else if ((id > 730 && id < 732) || id == 761) {
-      weatherIconElement.src = "/static/weatherIcons/dust-day.svg";
-      weatherIconElement.alt = "Day with dust whirls";
-    } else if (id > 740 && id < 742) {
-      weatherIconElement.src = "/static/weatherIcons/fog-day.svg";
-      weatherIconElement.alt = "Day with fog";
-    } else if (id == 751) {
-      weatherIconElement.src = "/static/weatherIcons/dust-day.svg";
-      weatherIconElement.alt = "Day with sand storms";
-    } else if (id == 762) {
-      weatherIconElement.src = "/static/weatherIcons/ash.svg";
-      weatherIconElement.alt = "Day with ash";
-    } else if (id == 771) {
-      weatherIconElement.src = "/static/weatherIcons/wind.svg";
-      weatherIconElement.alt = "Day with squalls";
-    } else if (id == 781) {
-      weatherIconElement.src = "/static/weatherIcons/tornado.svg";
-      weatherIconElement.alt = "Day with tornado";
-    } else if (id == 800) {
-      weatherIconElement.src = "/static/weatherIcons/clear-day.svg";
-      weatherIconElement.alt = "Day with clear sky";
-    } else {
-      weatherIconElement.src = "/static/weatherIcons/partly-cloudy-day.svg";
-      weatherIconElement.alt = "Cloudy day";
-    }
-  } else {
-    // Else if moon:
-    if (id > 199 && id < 203) {
-      weatherIconElement.src =
-        "/static/weatherIcons/thunderstorms-night-rain.svg";
-      weatherIconElement.alt = "Night with thunderstorm and rain";
-    } else if (id > 299 && id < 322) {
-      weatherIconElement.src = "/static/weatherIcons/thunderstorms-night.svg";
-      weatherIconElement.alt = "Night with thunderstorm";
-    } else if (id > 229 && id < 233) {
-      weatherIconElement.src =
-        "/static/weatherIcons/thunderstorms-night-snow.svg";
-      weatherIconElement.alt = "Night with thunderstorm and drizzle";
-    } else if (id > 299 && id < 322) {
-      weatherIconElement.src =
-        "/static/weatherIcons/partly-cloudy-night-drizzle.svg";
-      weatherIconElement.alt = "Night with drizzle";
-    } else if (id > 499 && id < 532) {
-      weatherIconElement.src =
-        "/static/weatherIcons/partly-cloudy-night-rain.svg";
-      weatherIconElement.alt = "Night with rain";
-    } else if (id > 599 && id < 603) {
-      weatherIconElement.src =
-        "/static/weatherIcons/partly-cloudy-night-snow.svg";
-      weatherIconElement.alt = "Night with snow";
-    } else if (id > 610 && id < 614) {
-      weatherIconElement.src =
-        "/static/weatherIcons/partly-cloudy-night-sleet.svg";
-      weatherIconElement.alt = "Night with sleet";
-    } else if (id > 614 && id < 623) {
-      weatherIconElement.src =
-        "/static/weatherIcons/partly-cloudy-night-sleet.svg";
-      weatherIconElement.alt = "Night with rain and snow";
-    } else if (id > 700 && id < 703) {
-      weatherIconElement.src = "/static/weatherIcons/mist.svg";
-      weatherIconElement.alt = "Night with mist";
-    } else if (id > 710 && id < 712) {
-      weatherIconElement.src = "/static/weatherIcons/smoke-particles.svg";
-      weatherIconElement.alt = "Night with smoke particles";
-    } else if (id > 720 && id < 723) {
-      weatherIconElement.src = "/static/weatherIcons/haze-night.svg";
-      weatherIconElement.alt = "Night with haze";
-    } else if ((id > 730 && id < 732) || id == 761) {
-      weatherIconElement.src = "/static/weatherIcons/dust-night.svg";
-      weatherIconElement.alt = "Night with dust whirls";
-    } else if (id > 740 && id < 742) {
-      weatherIconElement.src = "/static/weatherIcons/fog-night.svg";
-      weatherIconElement.alt = "Night with fog";
-    } else if (id == 751) {
-      weatherIconElement.src = "/static/weatherIcons/dust-night.svg";
-      weatherIconElement.alt = "Night with sand storms";
-    } else if (id == 762) {
-      weatherIconElement.src = "/static/weatherIcons/ash.svg";
-      weatherIconElement.alt = "Night with ash";
-    } else if (id == 771) {
-      weatherIconElement.src = "/static/weatherIcons/wind.svg";
-      weatherIconElement.alt = "Night with squalls";
-    } else if (id == 781) {
-      weatherIconElement.src = "/static/weatherIcons/tornado.svg";
-      weatherIconElement.alt = "Night with tornado";
-    } else if (id == 800) {
-      weatherIconElement.src = "/static/weatherIcons/starry-night.svg";
-      weatherIconElement.alt = "Night with clear sky";
-    } else {
-      weatherIconElement.src = "/static/weatherIcons/partly-cloudy-night.svg";
-      weatherIconElement.alt = "Cloudy night";
-    }
-  }
 });
 
 // Search city name by typing
@@ -298,7 +364,10 @@ form.addEventListener("submit", (event) => {
       } else {
         // else, display the weather info
         updateWeatherInfo(data[0]);
-        updateClock(data[0].timezone);
+        currentTime = updateClock(data[0].timezone);
+        let sunrise = data[0].sys.sunrise;
+        let sunset = data[0].sys.sunset;
+        updateWeatherIcon(data[0].weather[0].id, currentTime, sunrise, sunset);
 
         // Clear existing buttons
         historyContainer.innerHTML = "";
